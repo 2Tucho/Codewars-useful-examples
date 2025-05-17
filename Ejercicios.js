7de898f423030efd2b7d48e4f81a0a4585307c12
2Tucho <albergromero@gmail.com>
2025-05-17 23:09:11 +0200

    Added tranform string exercise

--- Ejercicios.js
+++ Ejercicios.js
@@ -459,3 +459,22 @@ function testit(s) {
 }
 
 testit("abc"); // "ac" (average of a(1) and b(2) is 1.5 → floor(1.5)=1. c is left alone so it adds up to the result as it is)
+
+// TRANSFORM STRINGS
+/* Solve the horror of unstandardized keypads by providing a function that converts computer input to a number as if it was typed on a phone.
+
+Example:
+"789" -> "123"
+
+Notes:
+You get a string with numbers only */
+function computerToPhone(numbers) {
+    let equivalence = {
+        0: 0, 1: 7, 2: 8, 3: 9, 4: 4, 5: 5, 6: 6, 7: 1, 8: 2, 9: 3
+    }
+    let result = ""
+    for (let i = 0; i < numbers.length; i++) {
+        result += equivalence[numbers[i]]
+    }
+    return result
+}
