9ded5ca96540c2e905f88f3e2f78b9659029508b
2Tucho <albergromero@gmail.com>
2025-05-22 10:15:35 +0200

    Added an array filter exercise

--- Ejercicios.js
+++ Ejercicios.js
@@ -551,3 +551,23 @@ function colourful(number) {
     return false
   } else return true
 }
+
+// ARRAY FILTER
+/* In this Kata, you will be given two strings a and b and your task will be to return the characters that are not common in the two strings.
+
+For example:
+
+solve("xyab","xzca") = "ybzc" 
+--The first string has 'yb' which is not in the second string. 
+--The second string has 'zc' which is not in the first string. 
+Notice also that you return the characters from the first string concatenated with those from the second string. */
+function solve(a, b) {
+  const arrA = a.split("");
+  const arrB = b.split("");
+  
+  //The filter searches each letter in the original string with the includes string method. It will leave only the letters that aren't there
+  const notCommonA = arrA.filter(letter => !b.includes(letter));
+  const notCommonB = arrB.filter(letter => !a.includes(letter));
+  
+  return notCommonA.concat(notCommonB).join("");
+}
