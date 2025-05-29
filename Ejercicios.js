2549c123f2f617eca8534269e1df7ba13e18e50c
2Tucho <albergromero@gmail.com>
2025-05-29 20:39:56 +0200

    Added an array transform exercise

--- Ejercicios.js
+++ Ejercicios.js
@@ -638,3 +638,31 @@ const balancedFilter = (arr) => {
     }
     return res
 }
+
+//ARRAY TRANSFORM
+/* Implement a function, so it will produce a sentence out of the given parts.
+
+Array of parts could contain:
+
+words;
+commas in the middle;
+multiple periods at the end.
+Sentence making rules:
+
+there must always be a space between words;
+there must not be a space between a comma and word on the left;
+there must always be one and only one period at the end of a sentence.
+Example:
+
+makeSentence(['hello', ',', 'my', 'dear']) // returns 'hello, my dear.' */
+function makeSentence(parts) {
+    let string = parts.filter(elem => elem !== " " && elem !== ".").join(" ")
+    let res = ""
+    for (let i = 0; i < string.length; i++) {
+        if (string[i + 1] == ",") {
+            null
+        } else res += string[i]
+    }
+
+    return res + "."
+}
