c473580a92ce178c6e9cdc720c15afdf9ca532b1
2Tucho <albergromero@gmail.com>
2025-06-03 12:42:17 +0200

    Added an array filter exercise

--- Ejercicios.js
+++ Ejercicios.js
@@ -666,3 +666,38 @@ function makeSentence(parts) {
 
     return res + "."
 }
+
+//ARRAY FILTER
+/* Greed is a dice game played with five six-sided dice. Your mission, should you choose to accept it, is to score a throw according to these rules. You will always be given an array with five six-sided dice values.
+
+ Three 1's => 1000 points
+ Three 6's =>  600 points
+ Three 5's =>  500 points
+ Three 4's =>  400 points
+ Three 3's =>  300 points
+ Three 2's =>  200 points
+ One   1   =>  100 points
+ One   5   =>   50 point
+A single die can only be counted once in each roll. For example, a given "5" can only count as part of a triplet (contributing to the 500 points) or as a single 50 points, but not both in the same roll.
+
+Example scoring
+
+ Throw       Score
+ ---------   ------------------
+ 5 1 3 4 1   250:  50 (for the 5) + 2 * 100 (for the 1s)
+ 1 1 1 3 1   1100: 1000 (for three 1s) + 100 (for the other 1)
+ 2 4 4 5 4   450:  400 (for three 4s) + 50 (for the 5)
+Note: your solution must not modify the input array. */
+function score(dice) {
+    let out = 0;
+    for (let j = 2; j <= 6; j++) {
+        if (dice.filter(i => i === j).length > 2) out += j * 100; //From 2-6 the only points given are for trios of each number
+    }
+    //The only exceptions are 1 and 5
+    let one = dice.filter(i => i === 1);
+    let five = dice.filter(i => i === 5);
+    if (one.length > 2) out += 1000; //For a 1 trio. 
+    out += (one.length % 3) * 100; //If 1 returns 1, if 2 returns 2, with 3 returns 0. It repeats since then (for 4 returns 1, 5 returns 2, 6 returns 0...)
+    out += (five.length % 3) * 50;
+    return out;
+}
