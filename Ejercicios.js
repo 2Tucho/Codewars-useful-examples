c2da8da9d36045f4a683eef106221bfcde994def
2Tucho <albergromero@gmail.com>
2025-06-03 13:34:25 +0200

    Added exercises from Daily Code Problem

--- Ejercicios.js
+++ Ejercicios.js
@@ -702,7 +702,7 @@ function score(dice) {
     return out;
 }
 
-//DAILY CODE PROBLEM (MEDIUM)
+//DAILY CODING PROBLEMS (MEDIUM)
 /* Write an algorithm to justify text. Given a sequence of words and an integer line length k, return a list of strings which represents each line, fully justified.
 
 More specifically, you should have as many words as possible in each line. There should be at least one space between each word. Pad extra spaces when necessary so that each line has exactly length k. Spaces should be distributed as equally as possible, with the extra spaces, if any, distributed starting from the left.
@@ -777,7 +777,7 @@ console.log(justifyText(words, k));
 // Output:
 // ["the  quick brown", "fox  jumps  over", "the   lazy   dog"]
 
-//DAILY CODE PROBLEM (EASY)
+//DAILY CODING PROBLEMS (EASY)
 /* Given a string of round, curly, and square open and closing brackets, return whether the brackets are balanced (well-formed).
 
 For example, given the string "([])[]({})", you should return true.
