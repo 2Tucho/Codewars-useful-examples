2f9a31069744c49fceb40c7a5ec3799aff5454ca
2Tucho <albergromero@gmail.com>
2025-07-01 16:23:29 +0200

    Added a daily coding problem exercise

--- Ejercicios.js
+++ Ejercicios.js
@@ -1074,3 +1074,8 @@ Given two strings, compute the edit distance between them. */
 /* Given a string, find the palindrome that can be made by inserting the fewest number of characters as possible anywhere in the word. If there is more than one palindrome of minimum length that can be made, return the lexicographically earliest one (the first one alphabetically).
 For example, given the string "race", you should return "ecarace", since we can add three letters to it (which is the smallest amount to make a palindrome). There are seven other palindromes that can be made from "race" by adding three letters, but "ecarace" comes first alphabetically.
 As another example, given the string "google", you should return "elgoogle". */
+
+//DAILY CODING PROBLEM (EASY)
+/* The power set of a set is the set of all its subsets. Write a function that, given a set, generates its power set.
+For example, given the set {1, 2, 3}, it should return {{}, {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}}.
+You may also use a list or array to represent a set. */
