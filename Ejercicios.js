26fc14e82b2a52b52934a0ddbeae9d5a67e64822
2Tucho <albergromero@gmail.com>
2025-07-11 13:55:25 +0200

    Added a daily coding problem exercise

--- Ejercicios.js
+++ Ejercicios.js
@@ -1105,6 +1105,9 @@ const editDistanceCounter = (a, b) => {
 }
 
 //DAILY CODING PROBLEM (EASY)
+/* Given an array of integers, determine whether it contains a Pythagorean triplet. Recall that a Pythogorean triplet (a, b, c) is defined by the equation a2+ b2= c2. */
+
+//DAILY CODING PROBLEM (EASY)
 /* Given a array of numbers representing the stock prices of a company in chronological order, write a function that calculates the maximum profit you could have made from buying and selling that stock once. You must buy before you can sell it.
 For example, given [9, 11, 8, 5, 7, 10], you should return 5, since you could buy the stock at 5 dollars and sell it at 10 dollars. */
 
