328b02c47672b45db8055bdf73e2e84f9475e0ee
2Tucho <albergromero@gmail.com>
2025-08-22 15:49:36 +0200

    Added a daily coding problem exercise

--- Ejercicios.js
+++ Ejercicios.js
@@ -1308,3 +1308,8 @@ For example, given n = 16, return 3 since we can make it with a 10¢, a 5¢, and
 
 //DAILY CODING PROBLEM (EASY)
 /* Write a program that checks whether an integer is a palindrome. For example, 121 is a palindrome, as well as 888. 678 is not a palindrome. Do not convert the integer into a string. */
+
+//DAILY CODING PROBLEM (MEDIUM)
+/* You are given n numbers as well as n probabilities that sum up to 1. Write a function to generate one of the numbers with its corresponding probability.
+For example, given the numbers [1, 2, 3, 4] and probabilities [0.1, 0.5, 0.2, 0.2], your function should return 1 10% of the time, 2 50% of the time, and 3 and 4 20% of the time.
+You can generate random numbers between 0 and 1 uniformly. */
