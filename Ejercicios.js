ceaf4c4730eddfe04c1fe5b719c6b51f947fdf5a
2Tucho <albergromero@gmail.com>
2025-09-06 08:45:06 +0200

    Added a daily coding problem exercise

--- Ejercicios.js
+++ Ejercicios.js
@@ -1389,3 +1389,6 @@ console.log(nthSevenish(7)); // 57 (49 + 7 + 1)
 
 //DAILY CODING PROBLEM (EASY)
 /* Given an array of numbers and a number k, determine if there are three entries in the array which add up to the specified number k. For example, given [20, 303, 3, 4, 25] and k = 49, return true as 20 + 4 + 25 = 49. */
+
+//DAILY CODING PROBLEM (MEDIUM)
+/* Given three 32-bit integers x, y, and b, return x if b is 1 and y if b is 0, using only mathematical or bit operations. You can assume b can only be 1 or 0. */
