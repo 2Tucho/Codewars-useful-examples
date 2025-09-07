043a50d668ba23fb0ec52e2575de59625c1af4a9
2Tucho <albergromero@gmail.com>
2025-09-07 14:04:04 +0200

    Aded a resolved daily coding exercise

--- Ejercicios.js
+++ Ejercicios.js
@@ -1392,3 +1392,7 @@ console.log(nthSevenish(7)); // 57 (49 + 7 + 1)
 
 //DAILY CODING PROBLEM (MEDIUM)
 /* Given three 32-bit integers x, y, and b, return x if b is 1 and y if b is 0, using only mathematical or bit operations. You can assume b can only be 1 or 0. */
+function select(x, y, b) {
+    let mask = -b; // mask is all ones if b=1, all zeros if b=0
+    return (x & mask) | (y & ~mask);
+}
