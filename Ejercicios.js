d0baf8d68989e1b080ab834d3081ff0d98d437f9
2Tucho <albergromero@gmail.com>
2025-09-12 09:53:30 +0200

    Added a daily coding problem exercise

--- Ejercicios.js
+++ Ejercicios.js
@@ -1484,7 +1484,12 @@ function printZigZag(s, k) {
     }
 }
 
-//
+//DAILY CODING PROBLEM (EASY)
 /* Given integers M and N, write a program that counts how many positive integer pairs (a, b) satisfy the following conditions:
 a + b = M
 a XOR b = N */
+
+//DAILY CODING PROBLEM (MEDIUM)
+/* Given an array of numbers, find the maximum sum of any contiguous subarray of the array.
+For example, given the array [34, -50, 42, 14, -5, 86], the maximum sum would be 137, since we would take elements 42, 14, -5, and 86.
+Given the array [-5, -1, -8, -9], the maximum sum would be 0, since we would not take any elements. */
