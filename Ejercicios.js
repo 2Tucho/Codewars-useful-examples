e7cd58eddd1b801b84db73de0202129f389f9524
2Tucho <albergromero@gmail.com>
2025-09-14 19:03:12 +0200

    Added a resolved daily coding problem

--- Ejercicios.js
+++ Ejercicios.js
@@ -1523,3 +1523,47 @@ You should print out the following:
 13
 12
  */
+function printSpiral(matrix) {
+    if (matrix.length === 0) return;
+    
+    let top = 0;
+    let bottom = matrix.length - 1;
+    let left = 0;
+    let right = matrix[0].length - 1;
+    let result = [];
+    
+    while (top <= bottom && left <= right) {
+        // Print top row
+        for (let i = left; i <= right; i++) {
+            result.push(matrix[top][i]);
+        }
+        top++;
+        
+        // Print right column
+        for (let i = top; i <= bottom; i++) {
+            result.push(matrix[i][right]);
+        }
+        right--;
+        
+        // Print bottom row (if applicable)
+        if (top <= bottom) {
+            for (let i = right; i >= left; i--) {
+                result.push(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+        
+        // Print left column (if applicable)
+        if (left <= right) {
+            for (let i = bottom; i >= top; i--) {
+                result.push(matrix[i][left]);
+            }
+            left++;
+        }
+    }
+    
+    // Print the result
+    for (const num of result) {
+        console.log(num);
+    }
+}
