5f48b52f60159ff18e2b56d281300097d2b413ea
2Tucho <albergromero@gmail.com>
2025-09-26 10:16:02 +0200

    Added a daily coding problem exercise

--- Ejercicios.js
+++ Ejercicios.js
@@ -1707,3 +1707,7 @@ function betterBoggle(board, dictionary) {
 
 console.log(betterBoggle(board, dictionary));
 // Output: ['cat', 'dog', 'desk', 'fish', 'red']
+
+//
+/* Given a string, determine whether any permutation of it is a palindrome.
+For example, carrace should return true, since it can be rearranged to form racecar, which is a palindrome. daily should return false, since there's no rearrangement that can form a palindrome. */
