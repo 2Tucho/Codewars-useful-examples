85029f9a31ed57de8269da04026557b8a32144d6
2Tucho <albergromero@gmail.com>
2025-10-02 14:42:48 +0200

    Added an array handler exercise

--- Ejercicios.js
+++ Ejercicios.js
@@ -801,6 +801,46 @@ function getSocks(name, socks) {
     }
 }
 
+//ARRAY HANDLER
+/* Sometimes we need information about the list/arrays we're dealing with. You'll have to write such a function in this kata. Your function must provide the following informations:
+Length of the array
+Number of integer items in the array
+Number of float items in the array
+Number of string character items in the array
+Number of whitespace items in the array
+The informations will be supplied in arrays that are items of another array. Like below:
+Output array = [[array length],[no of integer items],[no of float items],[no of string chars items],[no of whitespace items]]
+Added Difficulty
+If any item count in the array is zero, you'll have to replace it with a None/nil/null value (according to the language). And of course, if the array is empty then return 'Nothing in the array!. For the sake of simplicity, let's just suppose that there are no nested structures.
+Output ====== If you're head is spinning (just kidding!) then these examples will help you out- 
+array_info([1,2,3.33,4,5.01,'bass','kick',' '])--------->[[8],[3],[2],[2],[1]]    
+array_info([0.001,2,' '])------------------------------>[[3],[1],[1],[None],[1]]   
+array_info([])----------------------------------------->'Nothing in the array!'
+array_info([' '])-------------------------------------->[[1],[None],[None],[None],[1]] */
+function arrayInfo(arr){
+  let integer = 0
+  let float = 0
+  let string = 0
+  let whitespaces = 0
+  let res 
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === " ") {
+      whitespaces += 1
+    } else if(typeof arr[i] === "string" ) {
+      string += 1
+    } else if(typeof arr[i] === "number" && arr[i] % 1 == 0) {
+      integer += 1
+    } else if(typeof arr[i] === "number" && arr[i] % 1 !== 0) {
+      float += 1
+    } 
+  }
+  res = [[arr.length], [integer], [float], [string], [whitespaces]]
+  res = res.map(elem => elem[0] == 0 ? [null] : elem)
+  if(arr.length == 0) {
+    return "Nothing in the array!"
+  } else return res
+}
+
 //STRING TRANSFORM
 /* My friend wants a new band name for her band. She like bands that use the formula: "The" + a noun with the first letter capitalized, for example:
 "dolphin" -> "The Dolphin"
