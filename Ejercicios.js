6321489ce331a6d722042f497d41806058acedbb
2Tucho <albergromero@gmail.com>
2025-10-06 13:28:24 +0200

    Added a loop for exercise

--- Ejercicios.js
+++ Ejercicios.js
@@ -196,6 +196,20 @@ const shuffleArray = (arr) => {
     return shuffled;
 };
 
+// LOOP FOR
+/* Your task is to return the number of visible dots on a die after it has been rolled(that means the total count of dots that would not be touching the table when rolled)
+6, 8, 10, 12, 20 sided dice are the possible inputs for "numOfSides"
+topNum is equal to the number that is on top, or the number that was rolled.
+for this exercise, all opposite faces add up to be 1 more than the total amount of sides Example: 6 sided die would have 6 opposite 1, 4 opposite 3, and so on. for this exercise, the 10 sided die starts at 1 and ends on 10.
+Note: topNum will never be greater than numOfSides */
+function totalAmountVisible(topNum, numOfSides){
+  let total = 0
+  for(let i = 1; i <= numOfSides; i++) {
+    total += i
+  }
+  return total - ((numOfSides + 1) - topNum)
+}
+
 //FILTER AND TRANSFORM A STRING
 /* Given a string (str) containing a base-10 integer between 0 and 10000, convert the integer to its binary representation. At that point, obtain a count of the maximum amount of consecutive 0s. From there, return the count in written form with a capital letter.
 
